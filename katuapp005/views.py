0d9b34e2c3289fa0c0d14c62b436236afafb645e
Katuhito Hara <katuhitohara@gmail.com>
2021-06-11 12:02:10 +0900

    form

--- katuapp005/views.py
+++ katuapp005/views.py
@@ -1,21 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import HelloForm
 
 def index(request):
     params = {
         'title':'Hello/Index',
-        'msg':'This is sample pages',
-        'goto':'next',
+        'message':'your data:',
+        'form': HelloForm()   
     }
+    if (request.method == 'POST'):
+        params['message'] = '名前：' + request.POST['name'] + \
+            '<br>メール：' + request.POST['mail'] + \
+                '<br>年齢：' + request.POST['age']
+        params['form'] = HelloForm(request.POST)
     return render(request, 'katuapp005/index.html', params)
 
-def next(request):
-    params = {
-        'title':'Hello/Index',
-        'msg':'This is sample pages',
-        'goto':'index'
-    }
-    return render(request, 'katuapp005/index.html', params)
+
     
 
 
