3ff5c34c6b37d1ce4e8c4222e141341e2b9a42c7
Katuhito Hara <katuhitohara@gmail.com>
2021-06-14 17:21:34 +0900

    Form Fields

--- katuapp005/urls.py
+++ katuapp005/urls.py
@@ -1,6 +1,7 @@
+from django.conf.urls import url
 from django.urls import path
-from . import views
+from .views import HelloView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', HelloView.as_view(), name='index'),
 ]
