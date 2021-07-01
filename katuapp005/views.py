dacda6015d503867f24fe009a2c9bccd5f8d348f
Katuhito Hara <katuhitohara@gmail.com>
2021-07-01 16:38:21 +0900

    model

--- katuapp005/views.py
+++ katuapp005/views.py
@@ -5,6 +5,7 @@ from .models import Friend
 #from .forms import HelloForm
 from .forms import FriendForm
 from .forms import FindForm
+from django.db.models import Count, Sum, Avg, Min, Max
 
 #ジェネリックビュー
 from django.views.generic import ListView, DetailView
@@ -19,10 +20,20 @@ class FriendDetail(DetailView):
 #CURD:function base
 #index2
 def index(request):
+    # data = Friend.objects.all()
+    # data = Friend.objects.all().order_by('id')
     data = Friend.objects.all()
+    re1 = Friend.objects.aggregate(Count('age'))
+    re2 = Friend.objects.aggregate(Sum('age'))
+    re3 = Friend.objects.aggregate(Avg('age'))
+    re4 = Friend.objects.aggregate(Min('age'))
+    re5 = Friend.objects.aggregate(Max('age'))
+
+    msg = 'count:' + str(re1['age__count']) + '<br>Sum:' + str(re2['age__sum']) + '<br>Average:' + str(re3['age__avg']) + '<br>Min:' + str(re4['age__min']) + '<br>Max:' +str(re5['age__max'])
+
     params = {
         'title': 'Hello',
-        'msg': 'ALL Friends',
+        'message': msg,
         'data': data,
     }
     return render(request, 'katuapp005/index2.html', params)
@@ -70,9 +81,11 @@ def delete(request, num):
 #find
 def find(request):
     if (request.method == 'POST'):
+        msg = 'search result:'
         form = FindForm(request.POST)
         find = request.POST['find']
-        data = Friend.objects.filter(name__contains=find)
+        list = find.split()
+        data = Friend.objects.all()[int(list[0]):int(list[1])]
         msg = 'Result: ' + str(data.count())
     else:
         msg = 'search words...'
